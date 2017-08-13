ddef31d8c34e0159fb316c7869db2415b44af32a
mingang.he <dustgle@gmail.com>
2017-08-13 23:25:32 +0800

    Update

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 local_musics = []
 FIFO = '/tmp/quietbox.fifo'
 nbox = NEMbox.api.NetEase()
+player = NEMbox.player.Player()
 
 def get_163_playlist(playlist_id):
     songs = nbox.playlist_detail(playlist_id)
@@ -59,8 +60,32 @@ def wangye_play(id):
     return "not found"
 
 
+@app.route('/163/play_list/<int:id>', methods=['GET'])
+def wangye_play_list(id):
+    os.write(wfd, "\nP\n")
+    songs = nbox.playlist_detail(id)
+    detail_list = nbox.dig_info(songs, 'songs')
+    player.new_player_list('songs', "title", detail_list, -1)
+    player.end_callback = None
+    player.play_and_pause(0)
+    return "ok"
+
+
+@app.route('/163/prev', methods=['GET'])
+def wangye_prev():
+    player.prev()
+    return "ok"
+
+
+@app.route('/163/next', methods=['GET'])
+def wangye_next():
+    player.next()
+    return "ok"
+
+
 @app.route('/pause', methods=['GET'])
 def pause():
+    player.pause()
     os.write(wfd, "\nP\n")
     return "ok"
 
